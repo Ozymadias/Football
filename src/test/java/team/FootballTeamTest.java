1a414e8e4c23770186db3aaa6cfa89c799c3e516
Dominik <saidamyzo@gmail.com>
2017-12-27 17:54:10 +0100

    Add data provider to test

--- src/test/java/team/FootballTeamTest.java
+++ src/test/java/team/FootballTeamTest.java
@@ -1,15 +1,23 @@
 package team;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
 public class FootballTeamTest {
 
-    @Test
-    public void constructorShouldSetGamesWon() {
-        FootballTeam team = new FootballTeam(3);
-        assertEquals(team.getGamesWon(), 3,
-                "3 games were passed to constructor, but " + team.getGamesWon() + " were returned");
+    @DataProvider
+    private static final Object[][] nbOfGamesWon() {
+        return new Object[][]{
+                {0}, {1}, {2}, {3}
+        };
+    }
+
+    @Test(dataProvider = "nbOfGamesWon")
+    public void constructorShouldSetGamesWon(int nbOfGamesWon) {
+        FootballTeam team = new FootballTeam(nbOfGamesWon);
+        assertEquals(team.getGamesWon(), nbOfGamesWon,
+                nbOfGamesWon + " games were passed to constructor, but " + team.getGamesWon() + " were returned");
     }
 }
